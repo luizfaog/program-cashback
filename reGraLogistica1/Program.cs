2672ad827eb73e97772430948bac25fdc0378415
luizfaog <luiz.felipe008@hotmail.com>
2022-04-09 12:48:19 -0300

    program-cashback

---
+++ reGraLogistica1/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reGraLogistica1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcao;
+            double identificadorCerveja;
+            double identificadorDia;
+            Venda v = new Venda();
+            Console.WriteLine("---------- MENU ------------\n 1-Informar uma venda" +
+                "\n 2-Consultar o catálogo de cervejas" +
+                "\n 3-Sair");
+               
+            opcao = int.Parse(Console.ReadLine());
+
+            do
+            {
+                if (opcao == 1)
+                {
+                    Console.WriteLine("Infome o identificador da cerveja em que a venda foi realizada: \n 1-Skol \n 2-Brahma \n 3-Stella \n 4-Boehmia");
+                    identificadorCerveja = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Informe a quantidade de produtos vendidos: ");
+                    double quantidadeProd = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Informe o dia em que a venda foi realizada: \n 1-Domingo \n 2-Segunda \n 3-Terça  \n 4-Quarta \n 5-Quinta \n 6-Sexta \n 7-Sábado ");
+                    identificadorDia = double.Parse(Console.ReadLine());
+
+                    switch (identificadorCerveja)
+                    {
+                        case 1:
+                            Console.WriteLine("O valor de cashback para essa venda foi de: $$" + v.GetSkol(quantidadeProd, identificadorDia));
+                            break;
+                        case 2:
+                            Console.WriteLine("O valor de cashback para essa venda foi de: $$" + v.GetBrahma(quantidadeProd, identificadorDia));
+                            Console.ReadLine();
+                            break;
+                        case 3:
+                            Console.WriteLine("O valor de cashback para essa venda foi de: $$" + v.GetStella(quantidadeProd, identificadorDia));
+                            break;
+                        case 4:
+                            Console.WriteLine("O valor de cashback para essa venda foi de: $$" + v.GetBohemia(quantidadeProd, identificadorDia));
+                            break;
+                    }
+                    Console.WriteLine("---------- MENU ------------\n 1-Informar uma venda" +
+                "\n 2-Consultar o catálogo de cervejas" +
+                "\n 3-Sair");
+                    opcao = int.Parse(Console.ReadLine());
+                }            
+                else if (opcao == 2)
+                {
+                    Console.WriteLine("Infome o identificador da cerveja em que deseja consultar os valores de cashback: \n 1-Skol \n 2-Brahma \n 3-Stella \n 4-Boehmia");
+                    identificadorCerveja = double.Parse(Console.ReadLine());
+                    switch (identificadorCerveja)
+                    {
+                        case 1:
+                            Console.WriteLine("\n Tabela de cashback por dia da cerveja: SKOL" + 
+                                "\n DOMINGO | SEGUNDA | TERÇA | QUARTA | QUINTA | SEXTA | SÁBADO \n  25%    |    7%   |  6%   |    2%  |   10%  |  15%  |  20%  ");
+                            break;
+                        case 2:
+                            Console.WriteLine("\n Tabela de cashback por dia da cerveja: BRAHMA" +
+                                "\n DOMINGO | SEGUNDA | TERÇA | QUARTA | QUINTA | SEXTA | SÁBADO \n  30%    |    5%   | 10%   |   15%  |   20%  |  25%  |  30%  ");
+                            break;
+                        case 3:
+                            Console.WriteLine("\n Tabela de cashback por dia da cerveja: STELLA" +
+                                "\n DOMINGO | SEGUNDA | TERÇA | QUARTA | QUINTA | SEXTA | SÁBADO \n  35%    |    3%   |  5%   |    8%  |   13%  |  18%  |  25%  ");
+                            break;
+                        case 4:
+                            Console.WriteLine("\n Tabela de cashback por dia da cerveja: BOEHMIA" +
+                                "\n DOMINGO | SEGUNDA | TERÇA | QUARTA | QUINTA | SEXTA | SÁBADO \n  40%    |    10%  | 15%   |   15%  |   15%  |  20%  |  40%  ");
+                            break;
+                    }
+                    Console.WriteLine("---------- MENU ------------\n 1-Informar uma venda" +
+                "\n 2-Consultar o catálogo de cervejas" +
+                "\n 3-Sair");
+                    opcao = int.Parse(Console.ReadLine());
+                }
+                Console.ReadLine();
+                
+            }
+            while (opcao != 3);
+        }
+    }
+}
+
---
+++ reGraLogistica1/Venda.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reGraLogistica1
+{
+    class Venda
+    {
+        public double skol, Brahma, Stella, bohemia;
+        public Venda()
+        {
+            this.skol = 4.99;
+            this.Brahma = 4.5;
+            this.Stella = 4.15;
+            this.bohemia = 4.8;
+        }
+
+        public double GetSkol(double qtd, double dia)
+        {
+            double cashback = 0;
+            if (dia == 1)
+            {
+                cashback = ((qtd * skol) * 25) / 100;
+            }
+            else if (dia == 2)
+            {
+                cashback = ((qtd * skol) * 7) / 100;
+            }
+            else if (dia == 3)
+            {
+                cashback = ((qtd * skol) * 6) / 100;
+            }
+            else if (dia == 4)
+            {
+                cashback = ((qtd * skol) * 2) / 100;
+            }
+            else if (dia == 5)
+            {
+                cashback = ((qtd * skol) * 10) / 100;
+            }
+            else if (dia == 6)
+            {
+                cashback = ((qtd * skol) * 15) / 100;
+            }
+            else if (dia == 7)
+            {
+                cashback = ((qtd * skol) * 20) / 100;
+            }
+            return cashback;
+        }
+        public double GetBrahma(double qtd, double dia)
+        {
+            double cashback = 0;
+            if (dia == 1)
+            {
+                cashback = ((qtd * Brahma) * 30) / 100;
+            }
+            else if (dia == 2)
+            {
+                cashback = ((qtd * Brahma) * 5) / 100;
+            }
+            else if (dia == 3)
+            {
+                cashback = ((qtd * Brahma) * 10) / 100;
+            }
+            else if (dia == 4)
+            {
+                cashback = ((qtd * Brahma) * 15) / 100;
+            }
+            else if (dia == 5)
+            {
+                cashback = ((qtd * Brahma) * 20) / 100;
+            }
+            else if (dia == 6)
+            {
+                cashback = ((qtd * Brahma) * 25) / 100;
+            }
+            else if (dia == 7)
+            {
+                cashback = ((qtd * Brahma) * 30) / 100;
+            }
+            return cashback;
+        }
+        public double GetStella(double qtd, double dia)
+        {
+            double cashback = 0;
+            if (dia == 1)
+            {
+                cashback = ((qtd * Stella) * 35) / 100;
+            }
+            else if (dia == 2)
+            {
+                cashback = ((qtd * Stella) * 3) / 100;
+            }
+            else if (dia == 3)
+            {
+                cashback = ((qtd * Stella) * 5) / 100;
+            }
+            else if (dia == 4)
+            {
+                cashback = ((qtd * Stella) * 8) / 100;
+            }
+            else if (dia == 5)
+            {
+                cashback = ((qtd * Stella) * 13) / 100;
+            }
+            else if (dia == 6)
+            {
+                cashback = ((qtd * Stella) * 18) / 100;
+            }
+            else if (dia == 7)
+            {
+                cashback = ((qtd * Stella) * 25) / 100;
+            }
+            return cashback;
+        }
+        public double GetBohemia(double qtd, double dia)
+        {
+            double cashback = 0;
+            if (dia == 1)
+            {
+                cashback = ((qtd * bohemia) * 35) / 100;
+            }
+            else if (dia == 2)
+            {
+                cashback = ((qtd * bohemia) * 3) / 100;
+            }
+            else if (dia == 3)
+            {
+                cashback = ((qtd * bohemia) * 5) / 100;
+            }
+            else if (dia == 4)
+            {
+                cashback = ((qtd * bohemia) * 8) / 100;
+            }
+            else if (dia == 5)
+            {
+                cashback = ((qtd * bohemia) * 13) / 100;
+            }
+            else if (dia == 6)
+            {
+                cashback = ((qtd * bohemia) * 18) / 100;
+            }
+            else if (dia == 7)
+            {
+                cashback = ((qtd * bohemia) * 25) / 100;
+            }
+            return cashback;
+        }
+    }
+}
